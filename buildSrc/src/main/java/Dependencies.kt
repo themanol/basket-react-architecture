736566fe043ddd8c8f883c0e231aa7d9b5bf8c32
Manuel Garcia Rebollo <m.garcia.rebollo@mobgen.com>
2019-10-10 15:46:09 +0200

    [FIX] gradle configuration: invalid - character in android modules

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,87 +1,87 @@
 object ApplicationId{
-    val id = "com.themanol.reactbasket"
+    const val id = "com.themanol.reactbasket"
 }
 
 object Android {
-    val compileSdkVersion = 28
-    val buildToolsVersion = "29.0.0"
-    val minSdkVersion = 21
-    val targetSdkVersion = 28
-    val versionCode = 1
-    val versionName = "1.0"
+    const val compileSdkVersion = 28
+    const val buildToolsVersion = "29.0.0"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 28
+    const val versionCode = 1
+    const val versionName = "1.0"
 
 }
 
 object Modules {
-    val app = ":app"
-    val network = ":common:network"
-    val models = ":common:model"
-    val teamsDomain = ":features:teams:teams-domain"
-    val teamsPresentation = ":features:teams:teams-presentation"
-    val teamsData = ":features:teams:teams-data"
-    val teamsDI = ":features:teams:teams-di"
-    val gamesDomain = ":features:games:games-domain"
-    val gamesPresentation = ":features:games:games-presentation"
-    val gamesData = ":features:games:games-data"
-    val gamesDI = ":features:games:games-di"
-    val playersPresentation = ":features:players:players-presentation"
+    const val app = ":app"
+    const val network = ":common:network"
+    const val models = ":common:model"
+    const val teamsDomain = ":features:teams:teams-domain"
+    const val teamsPresentation = ":features:teams:teams_presentation"
+    const val teamsData = ":features:teams:teams-data"
+    const val teamsDI = ":features:teams:teams-di"
+    const val gamesDomain = ":features:games:games-domain"
+    const val gamesPresentation = ":features:games:games_presentation"
+    const val gamesData = ":features:games:games-data"
+    const val gamesDI = ":features:games:games-di"
+    const val playersPresentation = ":features:players:players_presentation"
 }
 
 object Versions {
-    val kotlin = "1.3.50"
-    val andoidX = "1.1.0"
-    val fragment = "1.2.0-alpha04"
-    val recyclerView = "1.0.0"
-    val constraintLayout = "1.1.3"
-    val andoidXLifeCycle = "2.1.0"
-    val andoidNavigation = "2.1.0"
-    val junit = "4.12"
-    val test_runner = "1.2.0"
-    val espresso = "3.2.0"
-    val gradle = "3.5.0"
-    val rxjava = "2.2.10"
-    val rxkotlin = "2.3.0"
-    val retrofit = "2.6.0"
-    val loggingInterceptor = "4.0.0"
-    val koin = "2.0.1"
+    const val kotlin = "1.3.50"
+    const val andoidX = "1.1.0"
+    const val fragment = "1.2.0-alpha04"
+    const val recyclerView = "1.0.0"
+    const val constraintLayout = "1.1.3"
+    const val andoidXLifeCycle = "2.1.0"
+    const val andoidNavigation = "2.1.0"
+    const val junit = "4.12"
+    const val test_runner = "1.2.0"
+    const val espresso = "3.2.0"
+    const val gradle = "3.5.0"
+    const val rxjava = "2.2.10"
+    const val rxkotlin = "2.3.0"
+    const val retrofit = "2.6.0"
+    const val loggingInterceptor = "4.0.0"
+    const val koin = "2.0.1"
 }
 
 object Plugins {
-    val kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
-    val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
-    val navigation_safe_args = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.andoidNavigation}"
+    const val kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val navigation_safe_args = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.andoidNavigation}"
 }
 
 object Libs {
-    val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.andoidX}"
-    val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
-    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-    val androidx_core = "androidx.core:core-ktx:${Versions.andoidX}"
-    val androidx_fragment = "androidx.fragment:fragment:${Versions.fragment}"
-    val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:${Versions.fragment}"
-    val androidx_viewmodels = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.andoidXLifeCycle}"
-    val androidx_livedata = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.andoidXLifeCycle}"
-    val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.andoidNavigation}"
-    val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.andoidNavigation}"
+    const val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.andoidX}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val androidx_core = "androidx.core:core-ktx:${Versions.andoidX}"
+    const val androidx_fragment = "androidx.fragment:fragment:${Versions.fragment}"
+    const val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:${Versions.fragment}"
+    const val androidx_viewmodels = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.andoidXLifeCycle}"
+    const val androidx_livedata = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.andoidXLifeCycle}"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.andoidNavigation}"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.andoidNavigation}"
 
-    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
-    val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava}"
+    const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlin}"
 
-    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-    val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
-    val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
-    val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+    const val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}"
 
-    val koinAndroid = "org.koin:koin-android:${Versions.koin}"
-    val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    const val koinAndroid = "org.koin:koin-android:${Versions.koin}"
+    const val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
 
 }
 
 object TestLibs {
-    val instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    val junit = "junit:junit:${Versions.junit}"
-    val test_runner = "androidx.test:runner:${Versions.test_runner}"
-    val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val test_runner = "androidx.test:runner:${Versions.test_runner}"
+    const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso}"
 }
 
