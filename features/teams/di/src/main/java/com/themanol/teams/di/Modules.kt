6b92aad71a300b8bd9a22151ba64d03c1a1e1949
Manuel Garcia Rebollo <m.garcia.rebollo@mobgen.com>
2019-10-18 14:58:10 +0200

    [ADD] add experimental annotations

--- features/teams/di/src/main/java/com/themanol/teams/di/Modules.kt
+++ features/teams/di/src/main/java/com/themanol/teams/di/Modules.kt
@@ -4,9 +4,13 @@ import com.themanol.reactbasket.teams.data.datasource.TeamDataSourceImpl
 import com.themanol.reactbasket.teams.data.datasource.TeamRemoteDataSource
 import com.themanol.reactbasket.teams.data.repository.TeamRepositoryImpl
 import com.themanol.reactbasket.teams.domain.repository.TeamRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+@ExperimentalCoroutinesApi
+@UseExperimental(FlowPreview::class)
 val repositoryModule: Module = module {
     single { TeamRepositoryImpl(remoteDataSource = get() ) as TeamRepository }
 }
